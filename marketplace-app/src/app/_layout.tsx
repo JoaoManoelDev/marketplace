23956f09948fdf77351a98ceb8011b0fc4d00c78
João Manoel <joaomanoeldev@gmail.com>
2026-03-08 12:09:49 -0300

    chore: config react query

--- marketplace-app/src/app/_layout.tsx
+++ marketplace-app/src/app/_layout.tsx
@@ -1,13 +1,18 @@
 import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import "../styles/global.css";
 
+const queryClient = new QueryClient();
+
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />  
-      <Stack.Screen name="login" />
-      <Stack.Screen name="register" />
-    </Stack>
+    <QueryClientProvider client={queryClient}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="register" />
+      </Stack>
+    </QueryClientProvider>
   )
 }
